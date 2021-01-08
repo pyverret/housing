72c9bec57a44647c62d705a30c917ce1da820a32
Pierre-Yves Verret <pyverret@gmail.com>
2021-01-07 22:22:41 -0500

    Compounding UI

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,4 @@
 import { Frequency } from '../enums/frequencies';
-import TextField from '@material-ui/core/TextField';
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
 import {useState, useEffect} from 'react';
 import {generateCompoundingInterest} from '../utils/compound';
 import { DataGrid, ColDef } from '@material-ui/data-grid';
@@ -79,47 +74,37 @@ function Home(): JSX.Element {
     setData(generateCompoundingInterest(inputs));
   }, [initial, years, compoundingFrequency, interest, contribution, contributionFrequency]);
 
-  return <>
+  return <div>
     <h1>Home</h1>
   
-    <form action="">
+    <form>
       <div>
-        <TextField id="initial-amount" label="Initial Amount" type="number" defaultValue={initial} onChange={(event) => setInitial(parseInt(event.target.value))} />
+        <label htmlFor="initial-amount">Initial Amount</label>
+        <input id="initial-amount" type="number" defaultValue={initial} onChange={(event) => setInitial(parseInt(event.target.value))}/>
       </div>
       <div>
-        <TextField id="years" label="Years" type="number" defaultValue={years} onChange={(event) => setYears(parseInt(event.target.value))} />
+        <label htmlFor="years">Years</label>
+        <input id="years" type="number" defaultValue={years} onChange={(event) => setYears(parseInt(event.target.value))}/>
       </div>
       <div>
-        <FormControl>
-          <InputLabel id="compounding-frequency">Compounding Frequency</InputLabel>
-          <Select
-            labelId="compounding-frequency"
-            id="compounding-frequency"
-            value={compoundingFrequency}
-            onChange={(event: React.ChangeEvent<{ value: any }>) => setCompoundingFrequency(parseInt(event.target.value))}
-            style={{width: "225px"}}>
-            {compoundingFrequencies.map((frequency) => <MenuItem value={frequency} key={`compounding-${frequency}`} data-testid="compounding-frequency">{Frequency[frequency]}</MenuItem>)}
-          </Select>
-        </FormControl>
+        <label htmlFor="compounding-frequency">Compounding Frequency</label>
+        <select name="compounding-frequency" id="compounding-frequency" defaultValue={compoundingFrequency} onChange={(event: React.ChangeEvent<{ value: any }>) => setCompoundingFrequency(parseInt(event.target.value))}>
+          {compoundingFrequencies.map((frequency) => <option value={frequency} key={`compounding-${frequency}`} data-testid="compounding-frequency">{Frequency[frequency]}</option>)}
+        </select>
       </div>
       <div>
-        <TextField id="interest" label="Interest" type="number" defaultValue={interest} onChange={(event) => setInterest(parseInt(event.target.value))} />
+        <label htmlFor="interest">Interest</label>
+        <input id="interest" type="number" defaultValue={interest} onChange={(event) => setInterest(parseInt(event.target.value))}/>
       </div>
       <div>
-        <TextField id="contribution-amount" label="Contribution Amount" type="number" defaultValue={contribution} onChange={(event) => setContribution(parseInt(event.target.value))} />
+        <label htmlFor="contribution-amount">Contribution Amount</label>
+        <input id="contribution-amount" type="text" defaultValue={contribution} onChange={(event) => setContribution(parseInt(event.target.value))}/>
       </div>
       <div>
-        <FormControl>
-          <InputLabel id="contribution-frequency">Contribution Frequency</InputLabel>
-          <Select
-            labelId="contribution-frequency"
-            id="contribution-frequency"
-            value={contributionFrequency}
-            onChange={(event: React.ChangeEvent<{ value: any }>) => setContributionFrequency(parseInt(event.target.value))}
-            style={{width: "225px"}}>
-            {contributionFrequencies.map((frequency) => <MenuItem value={frequency} key={`contribution-${frequency}`} data-testid="contribution-frequency">{Frequency[frequency]}</MenuItem>)}
-          </Select>
-        </FormControl>
+        <label htmlFor="contribution-frequency">Contribution Frequency</label>
+        <select name="contribution-frequency" id="contribution-frequency" defaultValue={contributionFrequency} onChange={(event: React.ChangeEvent<{ value: any }>) => setContributionFrequency(parseInt(event.target.value))}>
+          {contributionFrequencies.map((frequency) => <option value={frequency} key={`contribution-${frequency}`} data-testid="contribution-frequency">{Frequency[frequency]}</option>)}
+        </select>
       </div>
     </form>
 
@@ -156,8 +141,9 @@ function Home(): JSX.Element {
     <ul>
       <li>Periode: Periode in wich a change occured to the total amount, it can be a contribution or interest.</li>
     </ul>
+
     <DataGrid rows={data} columns={columns} pageSize={20} autoHeight />
-  </>;
+    </div>;
 }
 
 export default Home;
